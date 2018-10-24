fd30b9ce5c1161afbb98f12eb71b961d8b09d4b3
seongwonhan88 <seongwonhan88@gmail.com>
2018-10-24 10:59:54 +0900

    posts model migrated, Pillow installed

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    photo = models.ImageField(upload_to='post')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-pk']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    content = models.TextField('comment')
+    tags = models.ManyToManyField('HashTag',blank=True,)
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=50,)
+
+    def __str__(self):
+        return self.name
