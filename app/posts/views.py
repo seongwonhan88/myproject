3df69d5eeb046ca613a6145bf10521e776c71d4e
seongwonhan88 <seongwonhan88@gmail.com>
2018-10-24 11:32:53 +0900

    post_list view, url, html setup

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts':posts})
+
